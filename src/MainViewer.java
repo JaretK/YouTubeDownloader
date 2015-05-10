ed1a9d29c18645448c4e7fa67b18c9a0dfd21316
JaretK <jaret.karnuta@gmail.com>
2015-05-10 00:13:38 -0500

    Making progress on MainViewer... it needs a rename soon

--- src/MainViewer.java
+++ src/MainViewer.java
@@ -1,18 +1,13 @@
 
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -20,55 +15,96 @@ import javafx.stage.Stage;
 
 public class MainViewer extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	/*
+	 * controls debug operations throughout the program:
+	 * sets grid lines to active
+	 */
+	private static final boolean DEBUG = true;
+	/*
+	 * Controls the H and V spacing of the grid and the spacing of the 
+	 * buttons HBox
+	 */
+	private static final double SPACING = 10;
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Welcome to YouTubeDownloader");
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+	public static void main(String[] args) {
+		launch(args);
+	}
+	/*
+	 * Taken from:
+	 * http://docs.oracle.com/javase/8/javafx/get-started-tutorial/form.htm
+	 */
+	@Override
+	public void start(Stage primaryStage) {
+		//Set the title for our primary stage
+		primaryStage.setTitle("Welcome to YouTubeDownloader");
 
-        Text scenetitle = new Text("Welcome");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(scenetitle, 0, 0, 2, 1);
+		//Create the GridPane layout
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.TOP_CENTER);
+		grid.setHgap(SPACING);//H width between cols
+		grid.setVgap(SPACING);//V height between rows
+		/*
+		 * adds padding inset
+		 * constructor details:
+		 * Insets(double top, double right, double bottom, double left)
+		 * doubles represent pixels of padding
+		 */
+		grid.setPadding(new Insets(25,25,25,25));
 
-        Label userName = new Label("User Name:");
-        grid.add(userName, 0, 1);
+		//Add the scenetitle and text fields
+		Text scenetitle = new Text("Enter Song Information");
+		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		/*
+		Described within https://docs.oracle.com/javafx/2/api/javafx/scene/layout/GridPane.html as:
 
-        TextField userTextField = new TextField();
-        grid.add(userTextField, 1, 1);
+		add(Node child, int columnIndex, int rowIndex, int colspan, int rowspan)
+		Adds a child to the gridpane at the specified column,row position and spans.
 
-        Label pw = new Label("Password:");
-        grid.add(pw, 0, 2);
+		*/
+		grid.add(scenetitle, 0,0,2,1);
 
-        PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 2);
+		Label songName = new Label("Song Name:");
+		grid.add(songName, 0, 1);
+		TextField songNameTextField = new TextField();
+		grid.add(songNameTextField, 1, 1);
 
-        Button btn = new Button("Sign in");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 4);
+		Label artistName = new Label("Artist Name:");
+		grid.add(artistName, 0, 2);
+		TextField artistNameTextField = new TextField();
+		grid.add(artistNameTextField, 1, 2);
 
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
+		Label options = new Label("Options: ");
+		grid.add(options, 0,3);
+		TextField optionsTextField = new TextField();
+		grid.add(optionsTextField, 1, 3);
 
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+		//displays the gridLines for debugging
+		grid.setGridLinesVisible(DEBUG);
+		
+		/*
+		 * Make the exit and run buttons
+		 */
+		HBox buttonsHBOX = new HBox(SPACING);
+		Button quitBTN = new Button("Exit");
+		quitBTN.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		Button runBTN = new Button("Run");
+		runBTN.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		buttonsHBOX.setAlignment(Pos.BOTTOM_CENTER);
+		buttonsHBOX.getChildren().addAll(quitBTN, runBTN);
+		grid.add(buttonsHBOX,0,4,2,1);
 
-            @Override
-            public void handle(ActionEvent e) {
-                actiontarget.setFill(Color.FIREBRICK);
-                actiontarget.setText("Sign in button pressed");
-            }
-        });
+		/*
+		 * Scene constructor details:
+		 * Scene(Parent root, double width, double height)
+		 * Creates a scene for a specific root Node with a specific size
+		 * 
+		 * When width and height are not set, defaults to minimum size needed
+		 * to display all the elements
+		 */
+		//Scene scene = new Scene(grid, 300, 275);
+		Scene scene = new Scene(grid);
+		primaryStage.setScene(scene);
 
-        Scene scene = new Scene(grid, 300, 275);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+		primaryStage.show();
+	}
 }
